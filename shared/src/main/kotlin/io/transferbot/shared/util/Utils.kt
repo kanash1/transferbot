6a15e3acf66f1575fe6eaf703cacd55c6c6ae27a
kanash1 <oleg3038962@gmail.com>
2024-11-14 15:50:35 +0300

    new: map attachment to file

--- shared/src/main/kotlin/io/transferbot/shared/util/Utils.kt
+++ shared/src/main/kotlin/io/transferbot/shared/util/Utils.kt
@@ -1,10 +1,12 @@
 package io.transferbot.shared.util
 
 import io.transferbot.core.application.dto.AttachmentDto
+import io.transferbot.core.application.dto.AttachmentType
 import java.io.File
 import java.io.FileOutputStream
 import java.net.URL
 import java.nio.channels.Channels
+import kotlin.io.path.createTempFile
 
 fun File.writeFromAttachment(attachmentDto: AttachmentDto) {
     if (attachmentDto.url != null) {
@@ -14,5 +16,24 @@ fun File.writeFromAttachment(attachmentDto: AttachmentDto) {
         this.writeBytes(attachmentDto.file!!)
     } else {
         throw IllegalArgumentException("Attachment URL and file is null")
+    }
+}
+
+fun AttachmentDto.toFile(): File {
+    when (type) {
+        AttachmentType.PHOTO -> {
+            val filePath = createTempFile(suffix = ".jpg")
+            val file = filePath.toFile()
+            file.writeFromAttachment(this)
+            return file
+        }
+        AttachmentType.FILE -> {
+            val docNameLen = name?.length ?: 0
+            val lastDotIdx = name?.lastIndexOf('.') ?: docNameLen
+            val filePath = createTempFile(suffix = name?.substring(lastDotIdx, docNameLen))
+            val file = filePath.toFile()
+            file.writeFromAttachment(this)
+            return file
+        }
     }
 }
